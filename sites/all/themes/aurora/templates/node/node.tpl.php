f0cc7461d660078d8a2b7b307aadca049baa6b8f
Tim Hobert <timhobert@yahoo.com>
2014-12-09 13:01:36 -0500

    SASS CSS -- theming -- node tpl

--- sites/all/themes/aurora/templates/node/node.tpl.php
+++ sites/all/themes/aurora/templates/node/node.tpl.php
@@ -78,53 +78,33 @@
  * @see template_process()
  */
 ?>
-<?php if (!$page): ?>
-  <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-<?php endif; ?>
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php if ($user_picture || $display_submitted || !$page): ?>
+  <header>
+    <?php print render($title_prefix); ?>
     <?php if (!$page): ?>
-      <header>
-  <?php endif; ?>
+      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+  </header>
 
-      <div class="by-line">
-        <?php print $user_picture; ?>
-    
-        <?php print render($title_prefix); ?>
-        <?php if (!$page): ?>
-          <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
-    
-        <?php if ($display_submitted): ?>
-          <div class="submitted"><?php print $submitted; ?></div>
-        <?php endif; ?>
-      </div>
-
-    <?php if (!$page): ?>
-      </header>
-  <?php endif; ?>
+  <?php if ($display_submitted): ?>
+    <footer>
+      <?php print $user_picture; ?>
+      <p class="submitted"><?php print $submitted; ?></p>
+    </footer>
   <?php endif; ?>
 
   <div class="content"<?php print $content_attributes; ?>>
     <?php
-      // Hide comments, tags, and links now so that we can render them later.
+      // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
-      hide($content['field_tags']);
       print render($content);
     ?>
   </div>
 
-  <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
-    <div class="node-links">
-      <?php print render($content['field_tags']); ?>
-      <?php print render($content['links']); ?>
-    </div>
-  <?php endif; ?>
-
+  <?php print render($content['links']); ?>
   <?php print render($content['comments']); ?>
 
-<?php if (!$page): ?>
-  </article> <!-- /.node -->
-<?php endif; ?>
+</article>
