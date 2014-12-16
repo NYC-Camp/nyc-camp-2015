542ae3ae81c710378e6ad7820bb532fa39a278a8
Tim Hobert <timhobert@yahoo.com>
2014-12-16 13:24:32 -0500

    Theming -- font icons

--- sites/all/themes/nyc_theme/templates/system/html.tpl.php
+++ sites/all/themes/nyc_theme/templates/system/html.tpl.php
@@ -46,6 +46,8 @@
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
 
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
     <?php print $styles; ?>
     <?php print $scripts; ?>
 
