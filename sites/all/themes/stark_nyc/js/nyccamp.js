cd9b442bef921d4391572f5a744911f03790c482
Tim Hobert <tim@w2.local>
2015-03-10 20:27:08 -0400

    Theming -- add fitvids and fittext

---
+++ sites/all/themes/stark_nyc/js/nyccamp.js
@@ -0,0 +1,35 @@
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - http://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+
+(function ($, Drupal) {
+
+  // /* Responsive videos */
+  // Drupal.behaviors.fitVids = {
+  //   attach: function(context) {
+  //     $(document).ready(function(){
+  //       // Target your .container, .wrapper, .post, etc.
+  //       $(".page-wrapper").fitVids();
+  //     });
+  //   }
+  // };
+
+
+  // Your JavaScript code goes here:
+
+
+	$(document).ready(function() {
+
+		// Check if Firefox browser and add class to html.
+	  if ($.browser.mozilla) { $('html').addClass('mozilla'); }
+
+    // Target your .container, .wrapper, .post, etc.
+    $(".promo-caption-wrapper").fitText();
+
+    $("#main").fitVids();
+
+	});
+
+
+})(jQuery, Drupal);
